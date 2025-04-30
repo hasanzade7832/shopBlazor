37ef48f68e90c996aac20fb7bfd9a1ee2d8c70df
hoseyn7832 <hasanzade.hoseyn27@gmail.com>
2025-04-30 17:56:16 +0330

    added first commit

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ShobBlaze.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+    }
+}
