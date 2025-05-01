1c531a1e63daac61baf6d83a880ad0874087812d
hasanzade7832 <hasanzade.hoseyn27@gmail.com>
2025-05-01 12:32:03 +0330

    fasle5 completed

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public int Price { get; set; }
         public bool IsActive { get; set; }
-        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+        public List<Product_Prop> ProductProperties { get; set; } = new();
     }
 }
